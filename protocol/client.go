0cbe209eb35335e8df15ab21f54da2234cbfbc59
Yang Bai <hamo.by@gmail.com>
2014-09-11 13:16:57 +0800

    Basic client protocol

---
+++ protocol/client.go
@@ -0,0 +1,104 @@
+package protocol
+
+import (
+	"encoding/binary"
+	"net"
+
+	"tunnel"
+
+	"github.com/hamo/golog"
+)
+
+type Client struct {
+	username string
+
+	addressType byte
+	address     []byte
+	port        uint16
+
+	logger *golog.GoLogger
+}
+
+func NewClient(username string, addressType byte, address []byte, port uint16, logger *golog.GoLogger) *Client {
+	return &Client{
+		username:    username,
+		addressType: addressType,
+		address:     address,
+		port:        port,
+		logger:      logger,
+	}
+}
+
+func (c *Client) Upstream(client net.Conn, server tunnel.Writer) {
+	// FIXME: handle TCP/UDP flag
+	f := NewFlag()
+
+	switch c.addressType {
+	case 0x01: //IPv4
+		SetIPv4Flag(f)
+	case 0x03: //Domain
+		SetDomainFlag(f)
+	case 0x04: //IPv6
+		SetIPv6Flag(f)
+	}
+
+	l := len(c.username)
+	bl := byte(l & 0xFF)
+	nu := c.username[:bl]
+	if c.username != nu {
+		c.logger.Warningf("%s is too long. Trunc to %s", c.username, nu)
+	}
+	c.username = nu
+
+	mh := make([]byte, 1+bl+1)
+	mh[0] = bl
+	copy(mh[1:bl+1], c.username)
+	mh[bl+1] = *f
+
+	server.WriteMaster(mh)
+
+	uh := make([]byte, 1)
+	uh[0] = MagicByte
+
+	switch c.addressType {
+	case 0x01: //IPv4
+		fallthrough
+	case 0x04: //IPv6
+		uh = append(uh, c.address...)
+	case 0x03: //Domain
+		ali := len(c.address)
+		alb := byte(ali & 0xFF)
+		uh = append(uh, alb)
+		uh = append(uh, c.address...)
+	}
+
+	p := make([]byte, 2)
+	binary.BigEndian.PutUint16(p, c.port)
+
+	uh = append(uh, p...)
+
+	server.WriteUser(uh)
+
+	// FIXME: configurable buffer size
+	buf := make([]byte, 256)
+
+	for {
+		n, err := client.Read(buf)
+		server.WriteUser(buf[:n])
+		if err != nil {
+			break
+		}
+	}
+}
+
+func (c *Client) Downstream(client net.Conn, server tunnel.Reader) {
+	// FIXME: configurable buffer size
+	buf := make([]byte, 256)
+	for {
+		n, err := server.ReadUser(buf)
+		client.Write(buf[:n])
+		if err != nil {
+			break
+		}
+	}
+}
