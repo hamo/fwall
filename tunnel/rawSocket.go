cbd3765d6fcdbda08cfdb8f33f1d44bb82a7123f
madper <madper.xie@canonical.com>
2014-10-02 20:46:52 +0800

    Refactor. Adding default behavior for tunnels.
    Note: Here I added some default behavior for tunnels. All new tunnel
    should embed the base struct and *ONLY* override the matter methods.
    
    Signed-off-by: Madper <madper.xie@canonical.com>

--- tunnel/rawSocket.go
+++ tunnel/rawSocket.go
@@ -2,8 +2,6 @@ package tunnel
 
 import (
 	"fmt"
-	"io"
-	"net"
 
 	"encrypt"
 
@@ -11,40 +9,11 @@ import (
 )
 
 type RawSocketClient struct {
-	addr string
-	port int
-
-	iv      []byte
-	ivReady chan bool
-
-	masterKey []byte
-	password  []byte
-
-	crypto        *encrypt.CryptoInfo
-	masterEncrypt encrypt.Encryption
-	userEncryptR  encrypt.Encryption
-	userEncryptW  encrypt.Encryption
-
-	c net.Conn
-
-	logger *golog.GoLogger
+	ClientBase
 }
 
 type RawSocketServer struct {
-	iv      []byte
-	ivReady chan bool
-
-	masterKey []byte
-	password  []byte
-
-	crypto        *encrypt.CryptoInfo
-	masterEncrypt encrypt.Encryption
-	userEncryptR  encrypt.Encryption
-	userEncryptW  encrypt.Encryption
-
-	c net.Conn
-
-	logger *golog.GoLogger
+	ServerBase
 }
 
 func NewRawSocketClient(addr string, port int, masterKey string, encryptMethod string, password string, logger *golog.GoLogger) (*RawSocketClient, error) {
@@ -54,13 +23,15 @@ func NewRawSocketClient(addr string, port int, masterKey string, encryptMethod s
 	}
 
 	return &RawSocketClient{
-		addr:      addr,
-		port:      port,
-		crypto:    c,
-		ivReady:   make(chan bool, 0),
-		masterKey: c.GenKey(masterKey),
-		password:  c.GenKey(password),
-		logger:    logger,
+		ClientBase{
+			addr:      addr,
+			port:      port,
+			crypto:    c,
+			ivReady:   make(chan bool, 0),
+			masterKey: c.GenKey(masterKey),
+			password:  c.GenKey(password),
+			logger:    logger,
+		},
 	}, nil
 }
 
@@ -71,126 +42,15 @@ func NewRawSocketServer(masterKey string, encryptMethod string, logger *golog.Go
 	}
 
 	return &RawSocketServer{
-		crypto:    c,
-		ivReady:   make(chan bool, 0),
-		masterKey: c.GenKey(masterKey),
-		logger:    logger,
+		ServerBase{
+			crypto:    c,
+			ivReady:   make(chan bool, 0),
+			masterKey: c.GenKey(masterKey),
+			logger:    logger,
+		},
 	}, nil
 }
 
-func (r *RawSocketClient) Dial() error {
-	c, err := net.Dial("tcp", fmt.Sprintf("%s:%d", r.addr, r.port))
-	r.c = c
-	return err
-}
-
-func (r *RawSocketServer) Dial() error {
-	panic("server call Dial")
-}
-
-func (r *RawSocketClient) Accept(c net.Conn) {
-	panic("client call Accept")
-}
-
-func (r *RawSocketServer) Accept(c net.Conn) {
-	r.c = c
-}
-
-func (r *RawSocketClient) Close() {
-	r.c.Close()
-}
-
-func (r *RawSocketServer) Close() {
-	r.c.Close()
-}
-
-func (r *RawSocketClient) SetPassword(passwd string) {
-}
-
-//FIXME
-func (r *RawSocketServer) SetPassword(passwd string) {
-	r.password = r.crypto.GenKey(passwd)
-}
-
-func (r *RawSocketClient) ReadMaster(buf []byte, full bool) (int, error) {
-	panic("client call readMaster")
-}
-
-func (r *RawSocketServer) ReadMaster(buf []byte, full bool) (int, error) {
-	if r.masterEncrypt == nil {
-		// First call readMaster, so get IV first
-		iv := make([]byte, r.crypto.IVlen)
-		n, err := io.ReadFull(r.c, iv)
-		if err != nil {
-			close(r.ivReady)
-			return n, err
-		}
-		r.logger.Debugf("get IV: %v, err : %v", iv, err)
-		r.iv = iv
-		close(r.ivReady)
-		r.masterEncrypt = r.crypto.NewCrypto(r.iv, r.masterKey)
-	}
-
-	ciphertext := make([]byte, len(buf))
-	var n int
-	var err error
-
-	if full {
-		n, err = io.ReadFull(r.c, ciphertext)
-	} else {
-		n, err = r.c.Read(ciphertext)
-	}
-
-	plaintext := r.masterEncrypt.Decrypt(ciphertext[:n])
-
-	copy(buf[:n], plaintext[:n])
-	return n, err
-}
-
-func (r *RawSocketClient) ReadUser(buf []byte, full bool) (int, error) {
-	if r.userEncryptR == nil {
-		// call readuser from client, wait for iv ready
-		<-r.ivReady
-		r.userEncryptR = r.crypto.NewCrypto(r.iv, r.password)
-	}
-
-	ciphertext := make([]byte, len(buf))
-	var n int
-	var err error
-
-	if full {
-		n, err = io.ReadFull(r.c, ciphertext)
-	} else {
-		n, err = r.c.Read(ciphertext)
-	}
-
-	plaintext := r.userEncryptR.Decrypt(ciphertext[:n])
-
-	copy(buf[:n], plaintext[:n])
-	return n, err
-}
-
-func (r *RawSocketServer) ReadUser(buf []byte, full bool) (int, error) {
-	// call after readMaster
-	if r.userEncryptR == nil {
-		r.userEncryptR = r.crypto.NewCrypto(r.iv, r.password)
-	}
-
-	ciphertext := make([]byte, len(buf))
-	var n int
-	var err error
-
-	if full {
-		n, err = io.ReadFull(r.c, ciphertext)
-	} else {
-		n, err = r.c.Read(ciphertext)
-	}
-
-	plaintext := r.userEncryptR.Decrypt(ciphertext[:n])
-
-	copy(buf[:n], plaintext[:n])
-	return n, err
-}
 
 func (r *RawSocketClient) ReadContent(buf []byte) (int, error) {
 	if r.userEncryptR == nil {
@@ -227,45 +87,6 @@ func (r *RawSocketServer) ReadContent(buf []byte) (int, error) {
 
 	copy(buf[:n], plaintext[:n])
 	return n, err
-}
-
-func (r *RawSocketClient) WriteMaster(buf []byte) (int, error) {
-	if r.masterEncrypt == nil {
-		// first time to write master
-		r.iv = r.crypto.GenIV()
-		close(r.ivReady)
-		r.logger.Debugf("first write. IV: %v", r.iv)
-		r.c.Write(r.iv)
-
-		r.masterEncrypt = r.crypto.NewCrypto(r.iv, r.masterKey)
-	}
-
-	return r.c.Write(r.masterEncrypt.Encrypt(buf))
-}
-
-func (r *RawSocketServer) WriteMaster(buf []byte) (int, error) {
-	panic("Server call WriteMaster")
-}
-
-func (r *RawSocketClient) WriteUser(buf []byte) (int, error) {
-	if r.userEncryptW == nil {
-		r.userEncryptW = r.crypto.NewCrypto(r.iv, r.password)
-	}
-
-	ciphertext := r.userEncryptW.Encrypt(buf)
-
-	return r.c.Write(ciphertext)
-}
-
-func (r *RawSocketServer) WriteUser(buf []byte) (int, error) {
-	if r.userEncryptW == nil {
-		<-r.ivReady
-		r.userEncryptW = r.crypto.NewCrypto(r.iv, r.password)
-	}
-
-	ciphertext := r.userEncryptW.Encrypt(buf)
-
-	return r.c.Write(ciphertext)
 }
 
 func (r *RawSocketClient) WriteContent(buf []byte) (int, error) {
--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -3,6 +3,9 @@ package tunnel
 import (
 	"net"
 	"github.com/hamo/golog"
+	"encrypt"
+	"io"
+	"fmt"
 )
 
 type Reader interface {
@@ -64,4 +67,226 @@ func NewServer(serverType string, masterKey string, encryptMethod string, logger
 	default:
 		return NewRawSocketServer(masterKey, encryptMethod, logger)
 	}
+}
+
+// Embed Base behavior in your tunnel. Overriding them when needed.
+// Hope the following job will reduce the duplicate codes.
+type ClientBase struct {
+	addr string
+	port int
+
+	iv      []byte
+	ivReady chan bool
+
+	masterKey []byte
+	password  []byte
+
+	crypto        *encrypt.CryptoInfo
+	masterEncrypt encrypt.Encryption
+	userEncryptR  encrypt.Encryption
+	userEncryptW  encrypt.Encryption
+
+	c net.Conn
+
+	logger *golog.GoLogger
+}
+
+type ServerBase struct {
+	iv      []byte
+	ivReady chan bool
+
+	masterKey []byte
+	password  []byte
+
+	crypto        *encrypt.CryptoInfo
+	masterEncrypt encrypt.Encryption
+	userEncryptR  encrypt.Encryption
+	userEncryptW  encrypt.Encryption
+
+	c net.Conn
+
+	logger *golog.GoLogger
+}
+
+func (r *ClientBase) Dial() error {
+	c, err := net.Dial("tcp", fmt.Sprintf("%s:%d", r.addr, r.port))
+	r.c = c
+	return err
+}
+
+func (r *ServerBase) Accept(c net.Conn) {
+	r.c = c
+}
+
+func (r *ClientBase) Close() {
+	r.c.Close()
+}
+
+func (r *ServerBase) Close() {
+	r.c.Close()
+}
+
+func (r *ServerBase) Dial() error {
+	panic("server call Dial")
+}
+
+func (r *ClientBase) Accept(c net.Conn) {
+	panic("client call Accept")
+}
+
+func (r *ClientBase) SetPassword(passwd string) {
+	panic("client call SetPassword")
+}
+
+func (r *ServerBase) SetPassword(passwd string) {
+	r.password = r.crypto.GenKey(passwd)
+}
+
+func (r *ClientBase) ReadMaster(buf []byte, full bool) (int, error) {
+	panic("client call readMaster")
+}
+
+func (r *ServerBase) ReadMaster(buf []byte, full bool) (int, error) {
+	if r.masterEncrypt == nil {
+		// First call readMaster, so get IV first
+		iv := make([]byte, r.crypto.IVlen)
+		n, err := io.ReadFull(r.c, iv)
+		if err != nil {
+			close(r.ivReady)
+			return n, err
+		}
+		r.logger.Debugf("get IV: %v, err : %v", iv, err)
+		r.iv = iv
+		close(r.ivReady)
+		r.masterEncrypt = r.crypto.NewCrypto(r.iv, r.masterKey)
+	}
+
+	ciphertext := make([]byte, len(buf))
+	var n int
+	var err error
+
+	if full {
+		n, err = io.ReadFull(r.c, ciphertext)
+	} else {
+		n, err = r.c.Read(ciphertext)
+	}
+
+	plaintext := r.masterEncrypt.Decrypt(ciphertext[:n])
+
+	copy(buf[:n], plaintext[:n])
+	return n, err
+}
+
+func (r *ClientBase) ReadUser(buf []byte, full bool) (int, error) {
+	if r.userEncryptR == nil {
+		// call readuser from client, wait for iv ready
+		<-r.ivReady
+		r.userEncryptR = r.crypto.NewCrypto(r.iv, r.password)
+	}
+
+	ciphertext := make([]byte, len(buf))
+	var n int
+	var err error
+
+	if full {
+		n, err = io.ReadFull(r.c, ciphertext)
+	} else {
+		n, err = r.c.Read(ciphertext)
+	}
+
+	plaintext := r.userEncryptR.Decrypt(ciphertext[:n])
+
+	copy(buf[:n], plaintext[:n])
+	return n, err
+}
+
+func (r *ServerBase) ReadUser(buf []byte, full bool) (int, error) {
+	// call after readMaster
+	if r.userEncryptR == nil {
+		r.userEncryptR = r.crypto.NewCrypto(r.iv, r.password)
+	}
+
+	ciphertext := make([]byte, len(buf))
+	var n int
+	var err error
+
+	if full {
+		n, err = io.ReadFull(r.c, ciphertext)
+	} else {
+		n, err = r.c.Read(ciphertext)
+	}
+
+	plaintext := r.userEncryptR.Decrypt(ciphertext[:n])
+
+	copy(buf[:n], plaintext[:n])
+	return n, err
+}
+
+func (r *ClientBase) ReadContent(buf []byte) (int, error) {
+	plaintext := make([]byte, len(buf))
+	var n int
+	var err error
+
+	n, err = r.c.Read(plaintext)
+	copy(buf[:n], plaintext[:n])
+	return n, err
+}
+
+func (r *ServerBase) ReadContent(buf []byte) (int, error) {
+	// call after ParseUserHeader
+	plaintext := make([]byte, len(buf))
+	var n int
+	var err error
+
+	n, err = r.c.Read(plaintext)
+
+	copy(buf[:n], plaintext[:n])
+	return n, err
+}
+
+func (r *ClientBase) WriteMaster(buf []byte) (int, error) {
+	if r.masterEncrypt == nil {
+		// first time to write master
+		r.iv = r.crypto.GenIV()
+		close(r.ivReady)
+		r.logger.Debugf("first write. IV: %v", r.iv)
+		r.c.Write(r.iv)
+
+		r.masterEncrypt = r.crypto.NewCrypto(r.iv, r.masterKey)
+	}
+
+	return r.c.Write(r.masterEncrypt.Encrypt(buf))
+}
+
+func (r *ServerBase) WriteMaster(buf []byte) (int, error) {
+	panic("Server call WriteMaster")
+}
+
+func (r *ClientBase) WriteUser(buf []byte) (int, error) {
+	if r.userEncryptW == nil {
+		r.userEncryptW = r.crypto.NewCrypto(r.iv, r.password)
+	}
+
+	ciphertext := r.userEncryptW.Encrypt(buf)
+
+	return r.c.Write(ciphertext)
+}
+
+func (r *ServerBase) WriteUser(buf []byte) (int, error) {
+	if r.userEncryptW == nil {
+		<-r.ivReady
+		r.userEncryptW = r.crypto.NewCrypto(r.iv, r.password)
+	}
+
+	ciphertext := r.userEncryptW.Encrypt(buf)
+
+	return r.c.Write(ciphertext)
+}
+
+func (r *ClientBase) WriteContent(buf []byte) (int, error) {
+	return r.c.Write(buf)
+}
+
+func (r *ServerBase) WriteContent(buf []byte) (int, error) {
+	return r.c.Write(buf)
 }
