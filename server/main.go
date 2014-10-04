94a35e916c3ab4df9421337ed6a9e282bb1e035b
madper <madper.xie@canonical.com>
2014-10-04 14:53:36 +0800

    Revert "limit access for a given user." The map will cause a race. Must revert it.
    This reverts commit e753e5e46ea32976d3739eedd42e5a5e3f8844c3.

--- server/main.go
+++ server/main.go
@@ -26,27 +26,6 @@ var (
 	logger *golog.GoLogger
 )
 
-var UserTable map[string][]net.Addr // must be a race condition.
-
-// Note: please delete this func if you can find a built-in one.
-func addrInSlice(a net.Addr, list []net.Addr) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
-
-func addrPositionInSlice(a net.Addr, list []net.Addr) int {
-	for i, b := range list {
-		if b == a {
-			return i
-		}
-	}
-	return -1
-}
-
 func init() {
 	flDebug = flag.Bool("d", false, "debug switch")
 	flConfigFile = flag.String("c", "./config.json", "config file")
@@ -63,17 +42,6 @@ func handleConnection(c net.Conn) {
 	s := protocol.NewServer(nil)
 
 	user := s.Accept(r)
-	if UserTable[user] == nil {
-		UserTable[user] = make([]net.Addr, 1)
-		UserTable[user][0] = c.RemoteAddr()
-	} else if !addrInSlice(c.RemoteAddr(), UserTable[user]) {
-		// fixme: using configurable limits.
-		if len(UserTable[user]) >= 3 {
-			// fixme: adding debug info. Let user know why it failed.
-			return
-		}
-		UserTable[user] = append(UserTable[user], c.RemoteAddr())
-	}
 	ui, ok := udb.GetUserInfo(user)
 	if !ok {
 		c.Close()
@@ -100,14 +68,11 @@ func handleConnection(c net.Conn) {
 	s.Downstream(r, realServer)
 
 	realServer.Close()
-	pos := addrPositionInSlice(c.RemoteAddr(), UserTable[user])
-	UserTable[user] = append(UserTable[user][:pos], UserTable[user][pos+1:]...)
 	c.Close()
 }
 
 func main() {
 	var err error
-	UserTable = make (map[string][]net.Addr)
 
 	// FIXME: configurable logger file
 	logger = golog.New(os.Stdout)
